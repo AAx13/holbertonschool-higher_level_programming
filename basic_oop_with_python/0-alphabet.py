599a81e31bac23cb197fbfb32538cf0d3343e4d8
DamianAli <damian.ali33@gmail.com>
2016-05-17 12:19:32 -0700

    Finished task 1 (commented)

--- basic_oop_with_python/0-alphabet.py
+++ basic_oop_with_python/0-alphabet.py
@@ -1,7 +1,6 @@
-#! python
 
 alpha = [ "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" ]
 i = 0
 
 for i in range(0, len(alpha)): #this loop will use range method to go from 0 to the length of alpha
-    print "%s" % (alpha[i])   
+    print "%s" % (alpha[i])
---
+++ basic_oop_with_python/h_odd.py
@@ -0,0 +1,6 @@
+
+def odd(i):    # here im defining a function that will be called from another file.
+    if i % 2 == 0: # if the number passed as a parameter for a function called from another file is divisible by 2 then return 0.
+        return 0
+    else:
+        return 1

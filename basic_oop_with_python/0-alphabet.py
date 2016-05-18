9cd2b29cfd0486a9a34a13f24013b0ca9e44f58b
DamianAli <damian.ali33@gmail.com>
2016-05-18 11:17:43 -0700

    Swapping in more efficient code

--- basic_oop_with_python/0-alphabet.py
+++ basic_oop_with_python/0-alphabet.py
@@ -1,6 +1,8 @@
+import string
+#alpha = [ "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" ]
+#i = 0
 
-alpha = [ "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" ]
-i = 0
+#for i in range(0, len(alpha)): #this loop will use range method to go from 0 to the length of alpha
+#    print "%s" % (alpha[i])
 
-for i in range(0, len(alpha)): #this loop will use range method to go from 0 to the length of alpha
-    print "%s" % (alpha[i])
+print string.ascii_lowercase
---
+++ basic_oop_with_python/circle.py
@@ -0,0 +1,27 @@
+import math
+
+class Circle():
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def set_color(self, yellow):
+        self.__color = yellow
+
+    def get_color(self):
+        return self.__color
+
+    def set_center(self, center):
+        self.__center = center
+
+    def get_center(self):
+        return self.__center
+
+    def set_radius(self, radius):
+        self.__radius = radius
+
+    def get_radius(self):
+        return self.__radius
+
+    def area(self):
+        return self.__radius * self.__radius * math.pi

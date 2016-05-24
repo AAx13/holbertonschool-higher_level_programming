c06f075b837a87bc3941e80dc19f1520ce1acb00
DamianAli <damian.ali33@gmail.com>
2016-05-23 17:10:36 -0700

    Finished task 3 (commented)

--- swift_code_is_so_fast/3-longest_word.swift
+++ swift_code_is_so_fast/3-longest_word.swift
@@ -2,11 +2,8 @@ import Foundation
 
 func longest_word(text: String) -> (String) {
     let split_string = text.componentsSeparatedByString(" ")
-    var i = 0
-    for i in split_string {
-        if split_string[i].characters.count > split_string[i-1].characters.count {
-            var highest = split_string[i]
-        }
-    }
-    return highest
+    let max = split_string.maxElement({$1.characters.count > $0.characters.count})
+    return max!
 }
+
+// This script takes a sentence turns it into an array of strings and returns the longest word in the array

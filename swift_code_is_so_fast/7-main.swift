6a14519029c7018fe74b36ed7f5463d55a2ed651
DamianAli <damian.ali33@gmail.com>
2016-05-23 20:17:46 -0700

    Finished task 7 (commented)

--- swift_code_is_so_fast/7-main.swift
+++ swift_code_is_so_fast/7-main.swift
@@ -5,3 +5,15 @@ if is_prime(11) {
 if is_prime(20) {
     print("20 is prime")
 }
+if is_prime(101) {
+    print("101 is prime")
+}
+if is_prime(647) {
+    print("647 is prime")
+}
+if is_prime(929) {
+    print("929 is prime")
+}
+if is_prime(997) {
+    print("997 is prime")
+}
--- swift_code_is_so_fast/7-prime.swift
+++ swift_code_is_so_fast/7-prime.swift
@@ -1,4 +1,18 @@
-#!/usr/bin/swift
-func is_prime(number: Int) -> (Bool) {
-    
+import Foundation
+
+func is_prime(number: Int) -> (Bool)
+{
+    var i = 2
+    while i <= number - 1 {
+        if(number % i == 0) {
+        return false
+        }
+        i += 1
+    }
+    if(i == number) {
+        return true
+    }
+    return false
 }
+
+// This script will check a number and return True if its prime

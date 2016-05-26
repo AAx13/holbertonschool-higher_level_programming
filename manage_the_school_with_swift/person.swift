052136098b9b8d1ce5443f6392e5ba420c218458
DamianAli <damian.ali33@gmail.com>
2016-05-25 20:09:24 -0700

    Finished task 1 (commented)

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -1,22 +1,41 @@
 
 
 class Person {
-    // declaring all public attributes
+    // declaring all public attributes (task 0)
     var first_name: String
     var last_name: String
     var age: Int
     
-    // Initializing all public attributes
+    // Initializing all public attributes (task 0)
     init(first_name: String, last_name: String, age: Int) {
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
     }
     
-    // This function will join and return the first name and last name of the 'Person' provided
+    // This function will join and return the first name and last name of the 'Person' provided (task 0)
     func fullName() -> String {
         return self.first_name + " " + self.last_name
     }
     
+}
+
+protocol Classify {
+    func isStudent() -> Bool
+}
+
+// Defined two new sub-classes with the super class being Person (task 0)
+// Implementation of the isStudent protocol within the subclass Mentor (task 1)
+class Mentor: Person, Classify {
+    func isStudent() -> Bool {
+        return false
+    }
+}
+
+// Implementation of the isStudent protocol within the subclass Mentor (task 1)
+class Student: Person, Classify {
+    func isStudent() -> Bool {
+        return true
+    }
 }
 

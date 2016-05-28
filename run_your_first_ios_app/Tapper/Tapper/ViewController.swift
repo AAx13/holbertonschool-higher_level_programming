3decf0df57f2186236ec017552df7f9bafee9127
DamianAli <damian.ali33@gmail.com>
2016-05-28 14:21:47 -0700

    Finished this project

--- run_your_first_ios_app/Tapper/Tapper/ViewController.swift
+++ run_your_first_ios_app/Tapper/Tapper/ViewController.swift
@@ -11,30 +11,91 @@ import UIKit
 class ViewController: UIViewController {
 
     var taps_done = 0
-    var taps_requested = 0
+    var taps_requested:Int? = 0
     
     @IBOutlet weak var image_tapper: UIImageView!
     @IBOutlet weak var button_play: UIButton!
-    @IBOutlet weak var textfield_taps: UITextField!
     @IBOutlet weak var label_taps: UILabel!
     @IBOutlet weak var button_coin: UIButton!
+    @IBOutlet weak var textfield_number: UITextField!
     
     
-    
+    // This function will be called once button_play is clicked
     @IBAction func clickPlayButton(sender: AnyObject) {
         
-    }
-    
-    
-    @IBAction func clickCoinButton(sender: AnyObject) {
+        // Convert string to integer
+        self.taps_requested = Int(self.textfield_number.text!)
         
+        // Ensure input is bigger than 0 and is not nil
+        if taps_requested > 0 && taps_requested != nil {
+            print ("Let's do \(taps_requested) taps")
+            // Call initGame
+            initGame()
+        }
+    }
+
+    // This function will be called once button_coin is clicked
+    @IBAction func clickCoinButton(sender: AnyObject) {
+        // Increase the value of taps_done by one
+        taps_done += 1
+        
+        // Check value of taps_done
+        if taps_done >= taps_requested {
+            resetGame()
+        }
+        
+        // Update label_taps to display the value of taps_done
+        self.label_taps.text = String(self.taps_done) + "Taps !"
+        
+        // Print in the console "Tap!"
+        print ("Tap!")
     }
     
+    func initGame() {
+        // Hide: image_tapper, button_play and textfield_number
+        self.image_tapper.hidden = true
+        self.button_play.hidden = true
+        self.textfield_number.hidden = true
+        
+        // Show: label_taps, button_play
+        self.label_taps.hidden = false
+        self.button_coin.hidden = false
+        
+        // Reset taps_done to 0
+        taps_done = 0
+        
+        // Set the value of label_taps to "0 Taps"
+        self.label_taps.text = String(self.taps_done) + "Taps !"
+    }
     
+    func resetGame() {
+        //Hide: label_taps and button_coin
+        self.label_taps.hidden = true
+        self.button_coin.hidden = true
+        
+        //Show: image_tapper, button_play and `textfield_number
+        self.image_tapper.hidden = false
+        self.button_play.hidden = false
+        self.textfield_number.hidden = false
+        
+        //Reset taps_requested to 0
+        taps_requested = 0
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        taps_requested = 0
+        taps_done = 0
+        
+        //Hide: label_taps and button_coin
+        self.label_taps.hidden = true
+        self.button_coin.hidden = true
+        
+        //Show: image_tapper, button_play and `textfield_number
+        self.image_tapper.hidden = false
+        self.button_play.hidden = false
+        self.textfield_number.hidden = false
     }
 
     override func didReceiveMemoryWarning() {

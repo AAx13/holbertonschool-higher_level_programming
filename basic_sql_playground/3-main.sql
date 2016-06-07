720e4ddaa2e7ae5f381824a33a012ef4c3c9c733
DamianAli <damian.ali33@gmail.com>
2016-06-07 10:54:24 -0700

    Finished task 3

---
+++ basic_sql_playground/3-main.sql
@@ -0,0 +1,6 @@
+UPDATE Person SET age = 27 WHERE id = 6;
+UPDATE Person SET age = 18 WHERE id = 2;
+DELETE FROM Person WHERE id = 1;
+DELETE FROM EyesColor WHERE person_id = 1;
+SELECT * FROM Person
+ORDER BY first_name ASC;

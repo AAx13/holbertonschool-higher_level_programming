81a22bb923e562b84d5c6133386996d78ff771fc
DamianAli <damian.ali33@gmail.com>
2016-06-08 13:55:36 -0700

    Finished task 1

---
+++ silicon_valley_is_too_small_for_everybody/TechCompanies/TechCompanies/EntitiesHelper.swift
@@ -0,0 +1,38 @@
+//
+//  EntitiesHelper.swift
+//  TechCompanies
+//
+//  Created by Konflict on 6/8/16.
+//  Copyright © 2016 Holberton School. All rights reserved.
+//
+
+import Foundation
+
+class EntitiesHelper {
+    
+    static var listOfSchool:[Entity]! = []
+    static var listOfTechCompany:[Entity]! = []
+    
+    static func getSchools() -> [Entity]! {
+        
+        if listOfSchool.isEmpty {
+            listOfSchool.append(Entity(name: "Holberton", town: "San Francisco", imageName: "holberton", type: .School))
+        }
+        
+        return listOfSchool
+    }
+    
+    static func getTechCompanies() -> [Entity]! {
+        
+        if listOfTechCompany.isEmpty {
+            listOfTechCompany.append(Entity(name: "Linkedin", town: "San Francisco", imageName: "linkedin", type: .TechCompany))
+            listOfTechCompany.append(Entity(name: "Docker", town: "San Francisco", imageName: "docker", type: .TechCompany))
+            listOfTechCompany.append(Entity(name: "Google", town: "Mountain View", imageName: "google", type: .TechCompany))
+            listOfTechCompany.append(Entity(name: "Yahoo", town: "Sunnyvale", imageName: "yahoo", type: .TechCompany))
+            listOfTechCompany.append(Entity(name: "Apple", town: "Cupertino", imageName: "apple", type: .TechCompany))
+            listOfTechCompany.append(Entity(name: "Twitter", town: "San Francisco", imageName: "twitter", type: .TechCompany))
+        }
+        
+        return listOfTechCompany
+    }
+}

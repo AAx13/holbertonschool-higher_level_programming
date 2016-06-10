d3c7845511de0e8ebed20c63ff0a33aee1051be6
DamianAli <damian.ali33@gmail.com>
2016-06-10 13:56:04 -0700

    Finished task 0 (commented)

--- databases_are_so_easy_to_manage_in_command_line/main.py
+++ databases_are_so_easy_to_manage_in_command_line/main.py
@@ -1,12 +1,11 @@
 import sys
 
-actions = ['create', 'print', 'insert', 'delete']
+''' This script will check arguments passed and if it matches our arguments return it, otherwise return an error '''
+args = ['create', 'print', 'insert', 'delete']
 
 if len(sys.argv) < 2:
     print "Please enter an action"
+elif sys.argv[1] not in args:
+    print "Undefined action %s" % sys.argv[1]
 else:
-    for i in range(len(actions)):
-        if sys.argv[1] == actions[i]:
-            print actions[i]
-        elif sys.argv[1] != actions[i]:
-            print "Undefined action <%s>" % sys.argv[1]
+    print sys.argv[1]

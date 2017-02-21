d287dc79f0c61cc934c22fbf44ebc2791dffe50a
DamianAli <damian.ali33@gmail.com>
2017-02-21 15:23:32 -0800

    Task 6- running against hbtn checks.

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -21,10 +21,13 @@ class Rectangle():
     __width = 0
     __height = 0
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         ''' Constructor '''
         self.width = width
         self.height = height
+        type(self).number_of_instances += 1
 
     def __str__(self):
         ''' Overload __str__ to print rectangle. '''
@@ -39,6 +42,7 @@ class Rectangle():
     def __del__(self):
         ''' On delete print confirmation. '''
         print("Bye rectangle...")
+        type(self).number_of_instances -= 1
 
     @property
     def width(self):

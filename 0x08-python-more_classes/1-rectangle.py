628f5fbf2ad25bf774c240a18d9925947e3a9b4f
DamianAli <damian.ali33@gmail.com>
2017-02-20 20:04:50 -0800

    Task 1- running against hbtn check.

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+class Rectangle():
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    __width = 0
+    __height = 0
+
+    # width getter
+    @property
+    def width(self):
+        return self.__width
+
+    # width setter
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise Exception('width must be >= 0')
+            self.__width = value
+        else:
+            raise Exception('width must be an integer')
+
+    # height getter
+    @property
+    def height(self):
+        return self.__height
+
+    # height setter
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise Exception('height must be >=0')
+            self.__height = value
+        else:
+            raise Exception('height must be an integer')

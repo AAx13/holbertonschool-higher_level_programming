7b87a975d8c5202fd4c0d8471a3433d65ce6901f
DamianAli <damian.ali33@gmail.com>
2017-02-21 18:06:52 -0800

    Testing

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle():
         ''' Overload __str__ to print rectangle. '''
         if self.__width == 0 or self.__height == 0:
             return ''
-        return ((str(self.print_symbol) * self.__width + '\n') * self.__height).strip()
+        return ((str(self.print_symbol) * self.__width + '\n') * self.__height).strip('\n')
 
     def __repr__(self):
         ''' Overload __repr__ to return string. '''
